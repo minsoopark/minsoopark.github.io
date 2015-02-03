406189f029d6258de4a39b2770c4147693e5521c
Minsoo Park <minsoo1003@gmail.com>
2015-02-03 17:14:09 +0900

    profile animation removed

--- js/base.js
+++ js/base.js
@@ -1,7 +1,6 @@
 $(window).scroll(function () {
   var scrolled = $(window).scrollTop();
   if ($(window).width() <= 700) {
-    $(".img-profile").css("opacity", 1.0 - (scrolled / 150.0))
     $(".wrapper-nav-bar").css("opacity", (scrolled / 200.0))
   }
 });
